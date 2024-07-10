d679bdc782c9d04bf3d9f8bfb9b7aaeccde972df
Abdullahi Kabri <kabriacid01@gmail.com>
2024-07-10 19:24:02 +0100

    linked admin dashboard

--- subdomains/admin/inc/admin-sidebar.php
+++ subdomains/admin/inc/admin-sidebar.php
@@ -1,7 +1,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 pt-1 fixed-start ms-3 " id="sidenav-main">
    <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="">
+      <a class="navbar-brand m-0" href="admin-dashboard.php">
          <img src="../../assets/favicon/favicon.png" class="rounded shadow-lg navbar-brand-img h-100" alt="main_logo">
          <span class="ms-1 font-weight-bold">Amkadamiyya</span>
       </a>
