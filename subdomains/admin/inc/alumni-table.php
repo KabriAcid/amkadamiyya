bb06c3e68585e227f7becf95f9389a840222b5e5
Abdullahi Kabri <kabriacid01@gmail.com>
2024-07-18 18:34:59 +0100

    "Refactored alumni table HTML structure and styling in admin interface"

--- subdomains/admin/inc/alumni-table.php
+++ subdomains/admin/inc/alumni-table.php
@@ -18,29 +18,21 @@
                     <table class="table table-flush" id="datatable-basic">
                         <thead class="thead-light">
                             <tr>
-                                <th
-                                    class="text-uppercase text-left text-secondary text-xxs font-weight-bolder opacity-7">
+                                <th class="text-uppercase text-left text-secondary text-xxs font-weight-bolder opacity-7">
                                     Full Name</th>
-                                <th
-                                    class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
+                                <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
                                     Gender</th>
-                                <th
-                                    class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
+                                <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
                                     Phone Number</th>
-                                <th
-                                    class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
+                                <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
                                     Date of Birth</th>
-                                <th
-                                    class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
+                                <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
                                     State</th>
-                                <th
-                                    class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
+                                <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
                                     LGA</th>
-                                <th
-                                    class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
+                                <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
                                     Index No</th>
-                                <th
-                                    class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
+                                <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">
                                     Action</th>
                             </tr>
                         </thead>
@@ -48,42 +40,61 @@
                             <?php
                             if ($_SESSION['staff']['position_id'] == 1) {
                                 $sql = "SELECT * FROM `alumni` ORDER BY `first_name` ASC, `second_name` ASC, `index_no` ASC, `last_name` ASC, `gender` ASC, `state` ASC";
-
                                 $result = mysqli_query($conn, $sql);
                                 while ($row = mysqli_fetch_assoc($result)) {
-                                    ?>
+                            ?>
                                     <tr>
-                                        <td class="text-sm text-left font-weight-normal">
-                                            <?php echo $row['first_name'] . '&nbsp;' . $row['second_name'] . '&nbsp;' . $row['last_name'];
-                                            ; ?>
-
+                                        <td class="align-middle text-left text-sm">
+                                            <span class="text-secondary text-xs font-weight-bold text-capitalize">
+                                                <?php echo $row['first_name'] . '&nbsp;' . $row['second_name'] . '&nbsp;' . $row['last_name']; ?>
+                                            </span>
                                         </td>
-                                        <td class="text-sm text-center font-weight-normal"><?php echo $row['gender']; ?></td>
-                                        <td class="text-sm text-center font-weight-normal"><?php echo $row['phone_number']; ?>
+                                        <td class="align-middle text-center text-sm">
+                                            <span class="text-secondary text-xs font-weight-bold text-capitalize">
+                                                <?php echo $row['gender']; ?>
+                                            </span>
                                         </td>
-                                        <td class="text-sm text-center font-weight-normal"> <?php
-                                    $date_of_birth = date('d-M-Y', strtotime($row['date_of_birth']));
-                                    echo ucfirst($date_of_birth);
-                                    ?>
+                                        <td class="align-middle text-center text-sm">
+                                            <span class="text-secondary text-xs font-weight-bold text-capitalize">
+                                                <?php echo $row['phone_number']; ?>
+                                            </span>
                                         </td>
-                                        <td class="text-sm text-center font-weight-normal"><?php echo $row['state']; ?></td>
-                                        <td class="text-sm text-center font-weight-normal"><?php echo $row['lga']; ?></td>
-                                        <td class="text-sm text-center font-weight-normal">
-                                            <?php echo $row['index_no'];?>
+                                        <td class="align-middle text-center text-sm">
+                                            <span class="text-secondary text-xs font-weight-bold text-capitalize">
+                                                <?php
+                                                $date_of_birth = date('d-M-Y', strtotime($row['date_of_birth']));
+                                                echo ucfirst($date_of_birth);
+                                                ?>
+                                            </span>
                                         </td>
-                                        <td class="text-sm text-center font-weight-normal">
+                                        <td class="align-middle text-center text-sm">
+                                            <span class="text-secondary text-xs font-weight-bold text-capitalize">
+                                                <?php echo $row['state']; ?>
+                                            </span>
+                                        </td>
+                                        <td class="align-middle text-center text-sm">
+                                            <span class="text-secondary text-xs font-weight-bold text-capitalize">
+                                                <?php echo $row['lga']; ?>
+                                            </span>
+                                        </td>
+                                        <td class="align-middle text-center text-sm">
+                                            <span class="text-secondary text-xs font-weight-bold text-capitalize">
+                                                <?php echo $row['index_no']; ?>
+                                            </span>
+                                        </td>
+                                        <td class="align-middle text-center text-sm">
                                             <form action="admin-view-alumni.php" method="get">
                                                 <input type="hidden" name="alumni_id" value="<?php echo $row['alumni_id']; ?>">
                                                 <button type="submit" class="badge badge-sm rounded bg-gradient-light text-dark border-0">View</button>
                                             </form>
                                         </td>
                                     </tr>
-
-                                    <?php
+                            <?php
                                 }
                             }
                             ?>
                         </tbody>
+
                     </table>
                 </div>
             </div>
