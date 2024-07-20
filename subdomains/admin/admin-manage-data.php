e8921c5fd78911ff9f75f303ebe94abbad7d7ae9
Abdullahi Kabri <kabriacid01@gmail.com>
2024-07-20 13:03:10 +0100

    "Updated admin-manage-data.php: restructured form layout, added new form fields and buttons, and added a new card section for student levy management."

--- subdomains/admin/admin-manage-data.php
+++ subdomains/admin/admin-manage-data.php
@@ -19,29 +19,89 @@ require_once "../../config/database.php";
         <?php require "inc/admin-navbar.php"; ?>
         <div class="container-fluid pt-3">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-md-8">
                     <div class="card mb-4">
                         <!-- Card Header -->
                         <div class="card-header">
                             <h6 class="text-dark text-gradient">Manage Data</h6>
                             <p class="mb-0 text-sm">Here you can add and update various data entries such as bank names, disciplines, and qualifications.</p>
                         </div>
+                        <hr class="horizontal dark my-0">
                         <!-- Card Body -->
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group mb-3">
-                                        <label for="bank_name" class="form-label">Bank Name</label>
-                                        <input type="text" class="form-control" id="bank_name" name="bank_name" placeholder="Bank Name">
-                                    </div>
+                                <div class="col-12">
+                                    <form action="" method="post">
+                                        <div class="form-group">
+                                            <label for="bank_name" class="form-label">Bank Name</label>
+                                            <input type="text" class="form-control" id="bank_name" name="bank_name" placeholder="e.g Fidelity Bank" required>
+                                        </div>
+                                        <div class="form-group mb-3 text-end">
+                                            <button type="submit" class="btn bg-gradient-success btn-sm" name="add">Add</button>
+                                        </div>
+                                    </form>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group mb-3">
-                                        <label for="discipline" class="form-label">Disciplines</label>
-                                        <input type="text" class="form-control" name="discipline" placeholder="e.g Bsc.">
-                                    </div>
+                                <div class="col-12">
+                                    <form action="" method="post">
+                                        <div class="form-group">
+                                            <label for="discipline" class="form-label">Disciplines</label>
+                                            <input type="text" class="form-control" name="discipline" placeholder="e.g Statistics" required>
+                                        </div>
+                                        <div class="form-group mb-3 text-end">
+                                            <button type="submit" class="btn bg-gradient-success btn-sm" name="add">Add</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-12">
+                                    <form action="" method="post">
+                                        <div class="form-group">
+                                            <label for="discipline" class="form-label">Qualifications</label>
+                                            <input type="text" class="form-control" name="qualification" placeholder="e.g Bsc." required>
+                                        </div>
+                                        <div class="form-group text-end">
+                                            <button type="submit" class="btn bg-gradient-success btn-sm" name="add">Add</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <h6 class="text-dark text-gradient">Student Levy</h6>
+                            <p class="mb-0 text-sm">Here you can add and update student levy details, including various fees and charges applicable to students.</p>
+                        </div>
+                        <hr class="horizontal dark my-0">
+                        <div class="card-body">
+                            <form action="" method="post">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="discipline" class="form-label">Qualifications</label>
+                                            <input type="text" class="form-control" name="qualification" placeholder="e.g Bsc." required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="discipline" class="form-label">Qualifications</label>
+                                            <input type="text" class="form-control" name="qualification" placeholder="e.g Bsc." required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="discipline" class="form-label">Qualifications</label>
+                                            <input type="text" class="form-control" name="qualification" placeholder="e.g Bsc." required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group text-end">
+                                        <button type="submit" class="btn bg-gradient-success btn-sm" name="add">Add</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
