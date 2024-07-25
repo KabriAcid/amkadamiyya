780423f992c9199203551fa21d026a36cd504155
Abdullahi Kabri <kabriacid01@gmail.com>
2024-07-25 16:42:18 +0100

    "Added a blank line after requiring _CREATE.php in admin-add-subject.php"

--- subdomains/admin/admin-alumni-list.php
+++ subdomains/admin/admin-alumni-list.php
@@ -1,6 +1,25 @@
 <?php
 session_start();
 require_once "../../config/database.php";
+
+// Redirect function for convenience
+function redirect($url)
+{
+    header("Location: $url");
+    exit();
+}
+// Check if staff position ID is set
+if (isset($_SESSION['staff'])) {
+    $position_id = $_SESSION['staff']['position_id'];
+    // Hindering staff from editing part of their profile
+    $disabled = ''; // Default to no disabled attribute
+
+    if (!in_array($position_id, [1, 2, 3, 5])) {
+        redirect('admin-logout.php');
+    }
+} else {
+    redirect('admin-logout.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
