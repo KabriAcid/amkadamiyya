ca7a3afe005d077006ab7c3e337abcf54bb5778e
Abdullahi Kabri <kabriacid01@gmail.com>
2024-11-14 18:36:33 +0100

    added likes and comments functionality

--- pages/gallery/media-gallery.php
+++ pages/gallery/media-gallery.php
@@ -24,35 +24,65 @@ require '../../config/database.php';
                     $result = mysqli_query($conn, $sql);
                     if (mysqli_num_rows($result) > 0) {
                         while ($row = mysqli_fetch_assoc($result)) {
+                            // Get blog ID
+                            $blogId = $row['id'];
+
+                            // Count likes for this blog post
+                            $likesResult = mysqli_query($conn, "SELECT COUNT(*) AS like_count FROM blog_likes WHERE blog_id = $blogId");
+                            $likesCount = mysqli_fetch_assoc($likesResult)['like_count'];
+
+                            // Count comments for this blog post
+                            $commentsResult = mysqli_query($conn, "SELECT COUNT(*) AS comment_count FROM blog_comments WHERE blog_id = $blogId");
+                            $commentsCount = mysqli_fetch_assoc($commentsResult)['comment_count'];
+
+                            // Extract a preview of the blog content (18 words)
+                            $contentPreview = implode(' ', array_slice(explode(' ', $row['blog_content']), 0, 18));
                     ?>
-                            <div class="col-lg-10 mx-auto text-center">
+                            <div class="col-lg-8 mx-auto text-center">
                                 <div class="card card-blog card-plain">
                                     <div class="position-relative">
                                         <a class="d-block blur-shadow-image">
-                                            <img src="../../subdomains/admin/<?= $row['blog_thumbnail'] ?>" ; alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+                                            <img src="subdomains/admin/<?= $row['blog_thumbnail'] ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
                                         </a>
                                     </div>
                                     <div class="card-body px-0 pt-4">
                                         <p class="text-gradient text-info font-weight-bold text-sm text-uppercase">
-                                            <?= $row['blog_category']; ?></p>
+                                            <?= $row['blog_category']; ?>
+                                        </p>
                                         <a href="javascript:;">
                                             <h4>
                                                 <?= $row['blog_title']; ?>
                                             </h4>
                                         </a>
                                         <p>
-                                            <?= $row['blog_content']; ?>
+                                            <?= $contentPreview; ?>...
                                         </p>
-                                        <!-- <a href="pages/gallery/media-gallery.php" class="btn bg-gradient-info mt-3">Read more</a> -->
+                                        <a href="pages/gallery/media-gallery.php" class="btn bg-gradient-info mt-3">Read more</a>
+
+                                        <!-- Like and Comment Section -->
+                                        <div class="mt-3">
+                                            <form action="like.php" method="POST">
+                                                <input type="hidden" name="blog_id" value="<?= $blogId ?>">
+                                                <button type="submit" class="btn btn-sm btn-outline-primary">👍 Like (<?= $likesCount ?>)</button>
+                                            </form>
+                                            <span class="ml-2"><?= $commentsCount ?> Comments</span>
+                                        </div>
+
+                                        <!-- Comment Form -->
+                                        <form action="comment.php" method="POST" class="mt-3">
+                                            <input type="hidden" name="blog_id" value="<?= $blogId ?>">
+                                            <input type="text" name="user_name" placeholder="Your name" required class="form-control mb-2">
+                                            <textarea name="comment_text" placeholder="Add a comment" required class="form-control mb-2"></textarea>
+                                            <button type="submit" class="btn btn-sm btn-outline-success">Post Comment</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                     <?php
                         }
-                    } else {
-                        echo "0 results";
                     }
                     ?>
+
                 </div>
             </div>
         </section>
