4276a44871086eb4f1a8f5a37dc8b78b6ac6494a
Abdullahi Kabri <kabriacid01@gmail.com>
2024-11-14 15:29:40 +0100

    update media-gallery.php

--- pages/gallery/media-gallery.php
+++ pages/gallery/media-gallery.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require '../../config/database.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,8 +18,42 @@ session_start();
     <main>
         <section>
             <div class="container pt-5">
-                <h3 class="header">Media Gallery</h3>
-               
+                <div class="row">
+                    <?php
+                    $sql = 'SELECT * FROM blogs';
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                            <div class="col-lg-10 mx-auto text-center">
+                                <div class="card card-blog card-plain">
+                                    <div class="position-relative">
+                                        <a class="d-block blur-shadow-image">
+                                            <img src="../../subdomains/admin/<?= $row['blog_thumbnail'] ?>" ; alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+                                        </a>
+                                    </div>
+                                    <div class="card-body px-0 pt-4">
+                                        <p class="text-gradient text-info font-weight-bold text-sm text-uppercase">
+                                            <?= $row['blog_category']; ?></p>
+                                        <a href="javascript:;">
+                                            <h4>
+                                                <?= $row['blog_title']; ?>
+                                            </h4>
+                                        </a>
+                                        <p>
+                                            <?= $row['blog_content']; ?>
+                                        </p>
+                                        <!-- <a href="pages/gallery/media-gallery.php" class="btn bg-gradient-info mt-3">Read more</a> -->
+                                    </div>
+                                </div>
+                            </div>
+                    <?php
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    ?>
+                </div>
             </div>
         </section>
     </main>
